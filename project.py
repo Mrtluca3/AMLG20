bff3df125afb06c2309621b9080719cda2b7c3cd
Luca Martini <lmartini@login07.leonardo.local>
2026-04-26 20:48:36 +0200

    Create project.ipynb in local and then converted into .py by jupytext --sync prove.ipynb

---
+++ project.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: G20
+#     language: python
+#     name: g20kernel
+# ---
+
+# %%
+import torch
+import numpy 
+print(torch.__version__)
